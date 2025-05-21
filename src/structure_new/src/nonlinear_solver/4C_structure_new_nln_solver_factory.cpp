77cc83b047113808f006c1c69b25b91cdebf6e13
Vladimir Ivannikov <vladimir.ivannikov@hereon.de>
2025-05-21 13:41:22 +0200

    Modifty factory method to create a solver

--- src/structure_new/src/nonlinear_solver/4C_structure_new_nln_solver_factory.cpp
+++ src/structure_new/src/nonlinear_solver/4C_structure_new_nln_solver_factory.cpp
@@ -7,12 +7,12 @@
 
 #include "4C_structure_new_nln_solver_factory.hpp"
 
-#include "4C_structure_new_nln_solver_fullnewton.hpp"
 #include "4C_structure_new_nln_solver_nox.hpp"
-#include "4C_structure_new_nln_solver_ptc.hpp"
-#include "4C_structure_new_nln_solver_singlestep.hpp"
-#include "4C_structure_new_nln_solver_uzawa.hpp"
+#include "4C_structure_new_nln_solver_utils.hpp"
+#include "4C_structure_new_timint_base.hpp"
 #include "4C_utils_enum.hpp"
+
+#include <NOX_Utils.H>
 
 FOUR_C_NAMESPACE_OPEN
 
@@ -26,43 +26,60 @@ Solid::Nln::SOLVER::Factory::Factory()
 /*----------------------------------------------------------------------------*
  *----------------------------------------------------------------------------*/
 std::shared_ptr<Solid::Nln::SOLVER::Generic> Solid::Nln::SOLVER::Factory::build_nln_solver(
-    const enum Inpar::Solid::NonlinSolTech& nlnSolType) const
+    const enum Inpar::Solid::NonlinSolTech& nlnSolType,
+    const std::shared_ptr<Solid::TimeInt::BaseDataGlobalState>& gstate,
+    const std::shared_ptr<Solid::TimeInt::BaseDataSDyn>& sdyn,
+    const std::shared_ptr<Solid::TimeInt::NoxInterface>& noxinterface,
+    const std::shared_ptr<Solid::Integrator>& integrator,
+    const std::shared_ptr<const Solid::TimeInt::Base>& timint) const
 {
   std::shared_ptr<Solid::Nln::SOLVER::Generic> nlnSolver = nullptr;
 
-  switch (nlnSolType)
+  const auto it_set = settings.find(nlnSolType);
+
+  Teuchos::ParameterList predefined_nox_settings;
+
+  if (it_set != settings.end())
   {
-    case Inpar::Solid::soltech_newtonfull:
-      nlnSolver = std::make_shared<Solid::Nln::SOLVER::FullNewton>();
-      break;
-    case Inpar::Solid::soltech_nox_nln:
-      nlnSolver = std::make_shared<Solid::Nln::SOLVER::Nox>();
-      break;
-    case Inpar::Solid::soltech_ptc:
-      nlnSolver = std::make_shared<Solid::Nln::SOLVER::PseudoTransient>();
-      break;
-    case Inpar::Solid::soltech_singlestep:
-      nlnSolver = std::make_shared<Solid::Nln::SOLVER::SingleStep>();
-      break;
-    case Inpar::Solid::soltech_newtonuzawanonlin:
-    case Inpar::Solid::soltech_newtonuzawalin:
-      //      nlnSolver = Teuchos::rcp(new Solid::Nln::SOLVER::Uzawa());
-      //      break;
-    default:
-      FOUR_C_THROW("Solution technique \"{}\" is not implemented.", nlnSolType);
-      break;
+    predefined_nox_settings = it_set->second(*sdyn);
   }
 
-  return nlnSolver;
+  // ---------------------------------------------------------------------------
+  // STATUS TEST
+  // ---------------------------------------------------------------------------
+  /* This is only necessary for the special case, that you use no xml-file for
+   * the definition of your convergence tests, but you use the input file instead.
+   */
+  if (not is_xml_status_test_file(sdyn->get_nox_params().sublist("Status Test")))
+  {
+    std::set<enum NOX::Nln::StatusTest::QuantityType> qtypes;
+    Solid::Nln::SOLVER::create_quantity_types(qtypes, *sdyn);
+
+    // remove the unsupported quantity of status test:
+    // EAS is removed since it is an element
+    // quantity and not a nodal dof
+    qtypes.erase(NOX::Nln::StatusTest::quantity_eas);
+
+    Solid::Nln::SOLVER::set_status_test_params(
+        predefined_nox_settings.sublist("Status Test"), *sdyn, qtypes);
+  }
+
+  return std::make_shared<Solid::Nln::SOLVER::Nox>(
+      predefined_nox_settings, gstate, sdyn, noxinterface, integrator, timint);
 }
 
 /*----------------------------------------------------------------------------*
  *----------------------------------------------------------------------------*/
 std::shared_ptr<Solid::Nln::SOLVER::Generic> Solid::Nln::SOLVER::build_nln_solver(
-    const enum Inpar::Solid::NonlinSolTech& nlnSolType)
+    const enum Inpar::Solid::NonlinSolTech& nlnSolType,
+    const std::shared_ptr<Solid::TimeInt::BaseDataGlobalState>& gstate,
+    const std::shared_ptr<Solid::TimeInt::BaseDataSDyn>& sdyn,
+    const std::shared_ptr<Solid::TimeInt::NoxInterface>& noxinterface,
+    const std::shared_ptr<Solid::Integrator>& integrator,
+    const std::shared_ptr<const Solid::TimeInt::Base>& timint)
 {
   Factory factory;
-  return factory.build_nln_solver(nlnSolType);
+  return factory.build_nln_solver(nlnSolType, gstate, sdyn, noxinterface, integrator, timint);
 }
 
 FOUR_C_NAMESPACE_CLOSE
